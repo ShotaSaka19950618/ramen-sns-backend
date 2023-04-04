9f3246adcead636c06c02166608c2beb7e17b4b1
Shota.S <geminis218@icloud.com>
2023-04-04 16:02:27 +0900

    deploy

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// const userRouter = require("./routes/users");
-// const postRouter = require("./routes/posts");
-// const notificationRouter = require("./routes/notifications");
-// const authRouter = require("./routes/auth");
-// const uploadRouter = require("./routes/upload");
+const userRouter = require("./routes/users");
+const postRouter = require("./routes/posts");
+const notificationRouter = require("./routes/notifications");
+const authRouter = require("./routes/auth");
+const uploadRouter = require("./routes/upload");
 const PORT = 3000;
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const helmet = require("helmet");
 require("dotenv").config();
 
@@ -18,21 +18,21 @@ app.use(cors());
 app.use(helmet());
 
 // データベース接続
-// mongoose
-//   .connect(process.env.MONGOURL)
-//   .then(() => {
-//     console.log("DB接続中・・・");
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("DB接続中・・・");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // ミドルウェア
 app.use(express.json());
-// app.use("/api/users", userRouter);
-// app.use("/api/posts", postRouter);
-// app.use("/api/notifications", notificationRouter);
-// app.use("/api/auth", authRouter);
-// app.use("/api/upload", uploadRouter);
+app.use("/api/users", userRouter);
+app.use("/api/posts", postRouter);
+app.use("/api/notifications", notificationRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/upload", uploadRouter);
 
 app.listen(PORT, () => console.log("サーバーが起動しました"));
